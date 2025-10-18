f71592db39e92f0108a2d40fc2c0a006c59aca04
phani manda <phanimanda8@gmail.com>
2025-10-18 18:32:44 +0530

    update profile functionality added

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -95,7 +95,31 @@ export const login = async (req, res) => {
     }
 }
 
-export const logout = async (req, res) => {
+export const logout = async (_, res) => {
     res.cookie("jwt", "", {maxAge:0});
     res.status(200).json({message:"Logged out successfully"});
 };
+
+export const updateProfile = async (req, res) => {
+    try{
+        const { profilePic } = req.body;
+        if(!profilePic) return res.status(400).json({message:"Profile picture is required"});
+
+        const userId = req.user._id;
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        );
+
+        res.status(200).json(updatedUser);
+        } catch (error) {
+
+            console.log("Error in update profile:", error);
+            res.status(500).json({ message: "Internal server error" });
+        
+        }
+
+
+        
+    };
