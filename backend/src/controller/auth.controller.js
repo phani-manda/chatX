2d44544eaa197d014b2875b203882cfd8c1f4a97
phani manda <phanimanda8@gmail.com>
2025-10-18 16:46:09 +0530

    merging

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
+
     const { username, email, password } = req.body;
 
     try {
@@ -61,4 +62,35 @@ export const signup = async (req, res) => {
     
     }
 
+};
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message:"invalid credentials"});
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect) return res.status(400).json({message:"invalid credentials"});
+
+        generateToken(user._id, res)
+        res.status(200).json({
+            _id:user._id,
+            username:user.username,
+            email:user.email,
+            profilePic:user.profilePic,
+        });
+
+
+    } catch (error) {
+        console.error("Error in login controller:", error);
+        res.status(500).json({ message: "Internal server error" });
+        
+    }
+}
+
+export const logout = async (req, res) => {
+    res.cookie("jwt", "", {maxAge:0});
+    res.status(200).json({message:"Logged out successfully"});
 };
