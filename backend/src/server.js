569bcec549f276a4fa2cd620b41e067691b901eb
phani manda <phanimanda8@gmail.com>
2025-10-21 13:54:47 +0530

    added frontend config of signup page

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 
 
 const app = express();
@@ -15,6 +15,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // To parse JSON payloads
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,45 @@
 import {create} from 'zustand';
+import {axiosInstance} from '../lib/axios.js';
+import toast from 'react-hot-toast';
+
 
 export const useAuthStore = create((set) => ({
-    authUser: { name: "john", _id:123, age:25 },
-    isLoading: false,
+    authUser:null,
+    isCheckingAuth: true,
+    isSigningUp: false,
 
-    login: () => {
-        console.log("we just logged in");
-        set({ isLoading: true});
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser: res.data, isCheckingAuth: false});
+            
+        } catch (error) {
+            console.log("Error in authcheck:", error);
+            set({authUser: null, isCheckingAuth: false});  
+        }
+        finally{
+            set({isCheckingAuth: false});
+        }
+
     },
+
+    signup: async(data) => {
+        set({isSigningUp: true})
+        try {
+            const res = await axiosInstance.post("auth/signup", data);
+            set({authUser: res.data});
+
+            toast.success("Account created successfully!")
+            
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in signup:", error);
+            
+        } finally{
+            set({isSigningUp: false});
+        
+        }
+    }
+
 }));
