5d56038c582bdffa3254eb2fc2d3c8b10768ee6b
phani manda <phanimanda8@gmail.com>
2025-10-22 21:12:25 +0530

    finally adgjusted

--- frontend/src/components/NoConversationPlaceHolder.jsx
+++ frontend/src/components/NoConversationPlaceHolder.jsx
@@ -2,7 +2,7 @@ import { MessageCircleIcon } from "lucide-react";
 
 const NoConversationPlaceholder = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-center p-6">
+    <div className="flex-1 flex flex-col items-center justify-center text-center p-6">
       <div className="size-20 bg-cyan-500/20 rounded-full flex items-center justify-center mb-6">
         <MessageCircleIcon className="size-10 text-cyan-400" />
       </div>
