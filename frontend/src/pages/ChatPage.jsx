a0fad2395e80e9bbe2cb5a960924ca88b19962fd
phani manda <phanimanda8@gmail.com>
2025-10-22 16:13:51 +0530

    resolving

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,11 @@
 import { useChatStore } from "../store/useChatStore";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder.jsx";
 import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
 import ProfileHeader from "../components/ProfileHeader";
 import ActiveTabSwitch from "../components/ActiveTabSwitch";
 import ChatsList from "../components/ChatsList";
 import ContactList from "../components/ContactList";
 import ChatContainer from "../components/ChatContainer";
-import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
